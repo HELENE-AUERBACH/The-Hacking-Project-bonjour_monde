178a279e6cc12d35efc21b7a0fac799112bafdf3
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-04-21 20:36:09 +0200

    Ajout initial de tous les fichiers à l'exception du .env (grâce au .gitignore)

---
+++ lib/app.rb
@@ -0,0 +1,59 @@
+require 'twitter' # Appelle la gem Twitter
+require 'dotenv' # Appelle la gem Dotenv
+
+Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où est exécuté app.rb)
+                    # et, grâce à la gem Dotenv, ceci importe toutes les données enregistrées dans un hash ENV.
+
+def login_twitter
+  # Ces quelques lignes appellent les clés d'API du fichier .env
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+end
+
+def draw_of_names(total_number_of_names_to_choose)
+  # Tirage au sort de "total_number_of_names_to_choose" noms parmi la liste "journalist_array" (stockée sous forme de tableau) des noms de journalistes
+
+  journalist_array = ["@jcunniet", "@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain"]
+
+  journalist_sub_array = []
+  if !total_number_of_names_to_choose.nil? && total_number_of_names_to_choose.instance_of?(Integer) && total_number_of_names_to_choose > 0
+    total_number_of_journalists = journalist_array.length
+    generator_seed = Random.new_seed # Renvoie une graine arbitraire
+    generator = Random.new(generator_seed) # Crée le générateur pseudo-aléatoire de nombres entiers en l'initialisant à partir de la graine arbitraire
+    total_number_of_names_to_choose.times do # Tirage au sort des noms qui seront stockés dans la tableau "journalist_sub_array"
+      journalist_sub_array << journalist_array[generator.rand(total_number_of_journalists)]
+    end 
+  end
+  journalist_sub_array 
+end
+
+def generate_twitter_messages(journalist_sub_array, client)
+  if !journalist_sub_array.nil? && journalist_sub_array.instance_of?(Array) && journalist_sub_array.length > 0
+    question = "\#bonjour_monde \"peer-brainstorming\" : la solution parfaite pour suivre THP (@the_hacking_pro)? commencer par apprendre à devenir un dauphin!"
+    answer = "\#bonjour_monde Pourquoi? Afin d'avoir en permanence une moitié de notre cerveau active pendant que l'autre se reposera, et alterner. ;-p"
+    #client.update("Je te \"re-notifie\" ma blague pourrie de vendredi soir, @Toto, avec mon fameux hashtag!")
+    #client.update("@Toto \#TotoTataTitiTutu " + question)
+    #client.update("@Toto " + answer)
+    journalist_sub_array.length.times do |index|
+      #puts "client.update(\"#{journalist_sub_array[index]} #{question}\")"
+      client.update("#{journalist_sub_array[index]} #{question}")
+      sleep(5)
+      #puts "client.update(\"#{journalist_sub_array[index]} #{answer}\")"
+      client.update("#{journalist_sub_array[index]} #{answer}")
+    end
+  end   
+end
+
+client = login_twitter
+journalist_sub_array = draw_of_names(5)
+#puts "#{journalist_sub_array}"
+
+#require 'pry'
+#binding.pry # On lance PRY au milieu de la méthode afin de la "débugguer"
+# Ligne qui permet de tweeter à partir de mon compte un message dans l'onglet "Notifications" du user Twitter identifié par son "user_name"
+#client.update("Et maintenant, je te \"re-notifie\" toi, @Toto, avec un hashtag #TotoTataTitiTutu !")
+generate_twitter_messages(journalist_sub_array, client)
